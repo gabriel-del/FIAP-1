b0ee727cfe2e3a7bb2fcc3a00d033baeef97959c
Gabriel Del <gabrieldelc.barros@gmail.com>
2025-08-10 21:35:42 -0300

    fix: basePath

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   distDir: 'docs',
+  basePath: isGithubActions ? '/FIAP-1' : '',
+  assetPrefix: isGithubActions ? '/FIAP-1/' : '',
   images: {
     unoptimized: true
   }
